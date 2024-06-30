09f28c1fd273ed2b87433937993000810690a3cc
Yannick Oefler <yannick.oefler@students.uni-mannheim.de>
2024-06-30 15:33:55 +0200

    #1 main auskommentiert

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
-
 import torch
 
 from torchmeta.utils.data import BatchMetaDataLoader
-
 from common.args import parse_args
 from common.utils import get_optimizer, load_model
 from data.dataset import get_meta_dataset
@@ -11,61 +9,59 @@ from models.model import get_model
 from train.trainer import meta_trainer
 from utils import Logger, set_random_seed, cycle
 
-
 def main(rank, P):
     P.rank = rank
 
-    """ set torch device"""
+    """ Setze das Torch-Gerät """
     if torch.cuda.is_available():
         torch.cuda.set_device(P.rank)
     device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
 
-    """ fixing randomness """
+    """ Fixiere Zufälligkeit """
     set_random_seed(P.seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 
-    """ define dataset and dataloader """
+    """ Definiere Dataset und Dataloader """
     kwargs = {'batch_size': P.batch_size, 'shuffle': True,
               'pin_memory': True, 'num_workers': 2}
-    train_set, val_set = get_meta_dataset(P, dataset=P.dataset)
+    train_set, val_set = get_meta_dataset(P, dataset='income')
 
     train_loader = train_set
     test_loader = val_set
 
-    """ Initialize model, optimizer, loss_scalar (for amp) and scheduler """
+    """ Initialisiere Modell, Optimierer und Scheduler """
     model = get_model(P, P.model).to(device)
     optimizer = get_optimizer(P, model)
 
-    """ define train and test type """
+    """ Definiere Trainings- und Testtyp """
     from train import setup as train_setup
     from evals import setup as test_setup
     train_func, fname, today = train_setup(P.mode, P)
     test_func = test_setup(P.mode, P)
 
-    """ define logger """
+    """ Definiere Logger """
     logger = Logger(fname, ask=P.resume_path is None, today=today, rank=P.rank)
     logger.log(P)
     logger.log(model)
 
-    """ load model if necessary """
+    """ Lade Modell, falls notwendig """
     load_model(P, model, logger)
 
-    """ train """
+    """ Trainiere Modell """
     meta_trainer(P, train_func, test_func, model, optimizer, train_loader, test_loader, logger)
 
-    """ close tensorboard """
+    """ Schließe TensorBoard """
     logger.close_writer()
 
- 
 if __name__ == "__main__":
-    """ argument define """
+    """ Definiere Argumente """
     P = parse_args()
 
     P.world_size = torch.cuda.device_count()
     P.distributed = P.world_size > 1
     if P.distributed:
-        print("currently, ddp is not supported, should consider transductive BN before using ddp",
+        print("Currently, DDP is not supported, should consider transductive BN before using DDP",
               file=sys.stderr)
     else:
         main(0, P)
